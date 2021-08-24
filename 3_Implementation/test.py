0ea6d9ab4d0a84209bba96c17c35e053a893c800
Akash0299 <66462140+Akash0299@users.noreply.github.com>
2021-08-24 21:33:53 +0530

    Update test.py

--- 3_Implementation/test.py
+++ 3_Implementation/test.py
@@ -257,7 +257,7 @@ def test_quiz_answers_wrong(monkeypatch):
         assert True
 
 def test_snake_move_left(monkeypatch):
-    os.environ['SDL_VIDEODRIVER']='windlib'
+    os.environ['SDL_VIDEODRIVER']='dummy'
     monkeypatch.setattr('sys.stdin',pygame.K_LEFT)
     pygame.init()
     dis_width = 600
